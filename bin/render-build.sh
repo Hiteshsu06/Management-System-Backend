59e768f5c9c5c2aefb21c2721bd6f39066ad8aec
HiteshSukhpal03 <hiteshsukhpal03@gmail.com>
2024-09-02 23:34:21 +0530

    Added render build and yaml file

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,11 +3,11 @@
 set -o errexit
 
 bundle install
-# clean
-rm -rf public
-# build
-npm install --prefix client && npm run build --prefix client
-# migrate
-bundle exec rake db:migrate
-# postbuild
-cp -a client/build/. public/
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
+
+# If you're using a Free instance type, you need to
+# perform database migrations in the build command.
+# Uncomment the following line:
+
+# bundle exec rails db:migrate
