e486d5e1e72e5cf2e013df57a6c8af50b3eb5a77
HiteshSukhpal03 <hiteshsukhpal03@gmail.com>
2024-10-27 23:49:40 +0530

    remove google auth

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations',
-    passwords: 'users/passwords',
-    omniauth_callbacks: 'users/omniauth_callbacks'
+    passwords: 'users/passwords'
   }
   # Custom Routes
   get 'countries/list', to: 'indices#get_all_countries_list'
